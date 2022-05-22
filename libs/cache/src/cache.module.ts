8ee38fe09e0c9e588965be9608d78d85b8a1cb2e
justinemar <jmarcantado@gmail.com>
2022-05-23 02:04:56 +0800

    quick hack for node_env based config, fix ttl

--- libs/cache/src/cache.module.ts
+++ libs/cache/src/cache.module.ts
@@ -10,13 +10,23 @@ import * as redisStore from 'cache-manager-redis-store';
     CacheModuleNest.registerAsync({
         imports: [ConfigModule],
         inject: [ConfigService],
-        useFactory: async (configService: ConfigService) => ({
-            store: redisStore,
-            host: configService.get('REDIS_HOST'),
-            port: configService.get('REDIS_PORT'),
-            password: configService.get('REDIS_PASSWORD'),
-            ttl: configService.get('CACHE_TTL'),
-        })
+        useFactory: async (configService: ConfigService) => (
+        configService.get('NODE_ENV') != 'production' ?
+            {
+                store: redisStore,
+                host: configService.get('REDIS_HOST'),
+                port: configService.get('REDIS_PORT'),
+                ttl: configService.get('CACHE_TTL'),
+            }
+            :
+            {
+                store: redisStore,
+                host: configService.get('REDIS_HOST'),
+                port: configService.get('REDIS_PORT'),
+                password: configService.get('REDIS_PASSWORD'),
+                ttl: configService.get('CACHE_TTL'),
+            }
+        )
     })
 ]
 })
