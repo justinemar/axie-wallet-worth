6676db856d18550be80aed7d289cc85843c1391f
justinemar <jmarcantado@gmail.com>
2022-05-23 00:40:12 +0800

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication >(AppModule);
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
